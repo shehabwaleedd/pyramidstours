fea81fe775cb505f1ebc321c6e7e5de22cc5300a
Shehab Waleed <shehabwaleedd@gmail.com>
2024-04-17 02:16:27 +0200

    Add caching mechanism to serverUseToursByIds function

--- src/lib/tours/serverUseToursByIds.ts
+++ src/lib/tours/serverUseToursByIds.ts
@@ -1,13 +1,36 @@
+import { TourType } from "@/types/homePageTours";
+
+let cache: Record<string, TourType[]> = {};
+let cacheExpiry: number = Date.now() + 24 * 60 * 60 * 1000; // Set initial cache expiry to one day from now
+
 export async function serverUseToursByIds(query: string) {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/tour?${query}`);
+        // Check cache expiry and clear cache if needed
+        if (Date.now() > cacheExpiry) {
+            console.log("Cache expired, clearing cache");
+            cache = {};
+        }
+
+        // Return cached data if available
+        if (cache[query]) {
+            console.log("Returning cached data for query:", query);
+            return cache[query];
+        }
+
+        const questionAndQuery = query ? `?${query}` : "";
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/tour${questionAndQuery}`);
         if (!res.ok) {
             throw new Error(`Failed to fetch tours, status: ${res.status}`);
         }
         const data = await res.json();
-        return data.data.result; 
+        // Update cache and set new expiry
+        cache[query] = data.data.result;
+        cacheExpiry = Date.now() + 24 * 60 * 60 * 1000; 
+
+        return data.data.result;
     } catch (error) {
         console.error("Error fetching tours:", error);
-        return []; 
+
+        throw error;
     }
 }
