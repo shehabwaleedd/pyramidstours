d4a0b77bc01fe0c90570d6aeed97fc5675b3db32
Shehab Waleed <shehabwaleedd@gmail.com>
2024-04-23 17:27:58 +0200

    Update styles and options in page module and presets

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -15,7 +15,7 @@ import AccountHeaderNavbar from "@/components/accountHeaderNavbar/";
 import WishlistHeader from '../wishlistHeader';
 import SearchField from '../searchField';
 import DesktopMenu from './desktopMenu';
-import { opacity, background } from './anim';
+import { background } from './anim';
 
 const NavbarData = [
     {
@@ -34,14 +34,11 @@ const NavbarData = [
             { title: "Dahab Day Tours", href: "/dahab-day-tours" },
             { title: "Marsa Alam Day Tours", href: "/marsa-alam-day-tours" },
             { title: "El Gouna Day Tours", href: "/el-gouna-day-tours" },
-            { title: "Taba Day Tours", href: "/taba-day-tours" },
-            { title: "Nuweiba Day Tours", href: "/nuweiba-day-tours" },
             { title: "Siwa Day Tours", href: "/siwa-day-tours" },
             { title: "Bahariya Oasis Day Tours", href: "/bahariya-oasis-day-tours" },
             { title: "Fayoum Day Tours", href: "/fayoum-day-tours" },
             { title: "Safari Tours", href: "/safari-tours" },
             { title: "Honeymoon Tours", href: "/honeymoon-tours" },
-            { title: "Egypt and Jordan Tours", href: "/egypt-and-jordan-tours" },
         ]
     },
     {
@@ -50,15 +47,15 @@ const NavbarData = [
         href: "/packages",
         expandable: true,
         subMenu: [
-            { title: "2 Days 1 Night", href: "/2-days-1-night" },
-            { title: "3 Days 2 Nights", href: "/3-days-2-nights" },
-            { title: "4 Days 3 Nights", href: "/4-days-3-nights" },
-            { title: "5 Days 4 Nights", href: "/5-days-4-nights" },
-            { title: "6 Days 5 Nights", href: "/6-days-5-nights" },
-            { title: "7 Days 6 Nights", href: "/7-days-6-nights" },
-            { title: "8 Days 7 Nights", href: "/8-days-7-nights" },
-            { title: "9 Days 8 Nights", href: "/9-days-8-nights" },
-            { title: "10 Days 9 Nights", href: "/10-days-9-nights" },
+            { title: "2 Days 1 Night", href: "/2-days-1-night-tours" },
+            { title: "3 Days 2 Nights", href: "/3-days-2-nights-tours" },
+            { title: "4 Days 3 Nights", href: "/4-days-3-nights-tours" },
+            { title: "5 Days 4 Nights", href: "/5-days-4-nights-tours" },
+            { title: "6 Days 5 Nights", href: "/6-days-5-nights-tours" },
+            { title: "7 Days 6 Nights", href: "/7-days-6-nights-tours" },
+            { title: "8 Days 7 Nights", href: "/8-days-7-nights-tours" },
+            { title: "9 Days 8 Nights", href: "/9-days-8-nights-tours" },
+            { title: "10 Days 9 Nights", href: "/10-days-9-nights-tours" },
         ]
     },
     {
@@ -71,7 +68,6 @@ const NavbarData = [
             { title: "Sharm El Sheikh Airport Transfers", href: "/sharm-el-sheikh-airport-transfers" },
             { title: "Hurghada Airport Transfers", href: "/hurghada-airport-transfers" },
             { title: "Luxor Airport Transfers", href: "/luxor-airport-transfers" },
-            { title: "Marsa Alam Airport Transfers", href: "/marsa-alam-airport-transfers" },
             { title: "Alexandria Airport Transfers", href: "/alexandria-airport-transfers" },
         ],
     },
