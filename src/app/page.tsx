c4c6575cf1be525ad5cbeb1ede96f0e8788f5cfa
Shehab Waleed <shehabwaleedd@gmail.com>
2024-05-02 08:35:51 +0300

    Refactor Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,22 @@
-'use client'
-import React, { useState, useEffect } from 'react'
-import { useAuth } from "@/context/AuthContext";
+
+import React from 'react'
 import styles from "./page.module.scss";
 import TestimonialsCards from "@/components/testimonialHomePage"
 import SearchForm from "@/components/landing";
 import { lazy, Suspense } from "react";
 import Skeleton from "@/animation/skeleton";
 const ToursHomePage = lazy(() => import('@/components/toursHomePage'));
-import Opening from "@/animation/opening";
-
 export default function Home() {
-  const { hasAnimationShown, setHasAnimationShown } = useAuth();
-  const [renderOpening, setRenderOpening] = useState(false);
-
-  useEffect(() => {
-    if (!hasAnimationShown && !sessionStorage.getItem("hasAnimationShown")) {
-      setRenderOpening(true);
-    }
-  }, [hasAnimationShown])
 
 
 
   return (
-    <>
-      {/* {renderOpening && <Opening setHasAnimationShown={setHasAnimationShown}/>} */}
-
-      <main className={styles.main}>
-        <SearchForm />
-        <Suspense fallback={<Skeleton />}>
-          <ToursHomePage />
-        </Suspense>
-        <TestimonialsCards />
-      </main>
-    </>
+    <main className={styles.main}>
+      <SearchForm />
+      <Suspense fallback={<Skeleton />}>
+        <ToursHomePage />
+      </Suspense>
+      <TestimonialsCards />
+    </main>
   );
 }
