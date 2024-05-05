1ca8fef0f3bfdcf6641157423e91b0c3e56f0066
Shehab Waleed <shehabwaleedd@gmail.com>
2024-05-05 10:00:24 +0300

    Refactor imports in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@
 import React from 'react'
 import styles from "./page.module.scss";
 import SearchForm from "@/components/landing";
-import { lazy, Suspense } from "react";
-import Skeleton from "@/animation/skeleton";
-const ToursHomePage = lazy(() => import('@/components/toursHomePage'));
-const TestimonialsCards = lazy(() => import('@/components/testimonialHomePage'));
+import ToursHomePage from '@/components/toursHomePage';
+import TestimonialsCards from '@/components/testimonialHomePage';
 export default function Home() {
 
 
@@ -13,10 +11,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <SearchForm />
-      <Suspense fallback={<Skeleton />}>
-        <ToursHomePage />
-        <TestimonialsCards />
-      </Suspense>
+      <ToursHomePage />
+      <TestimonialsCards />
     </main>
   );
 }
